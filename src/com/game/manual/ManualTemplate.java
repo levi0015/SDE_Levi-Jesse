42b1546ff160b880dfbb4c955b018142dbece23f
Jessevdbos <bos0038@hz.nl>
2023-01-19 15:26:14 +0100

    added manual description

--- src/com/game/manual/ManualTemplate.java
+++ src/com/game/manual/ManualTemplate.java
@@ -12,7 +12,8 @@ abstract public class ManualTemplate {
     }
 
     public String intro() {
-        return "Thank you for choosing one of our " + this.blueprint.get(0) + "'s. We are happy to have you be a customer of our services.\n" +
+        return "Manual:\n" +
+                "Thank you for choosing one of our " + this.blueprint.get(0) + "'s. We are happy to have you be a customer of our services.\n" +
                 "We would like you to know that our " + this.blueprint.get(0) + " collection is spreading globally and we are expending our services across the world.\n" +
                 "Now let's give a bit of information about your " + this.blueprint.get(0) + ":\n";
     };
